59a8acc25afa911cabbe43047d4b2b5e299121b9
Bhaveshgupta00 <bhaveshguptanit@gmail.com>
2025-03-28 10:02:51 +0530

    Product page

---
+++ frontend/src/app/material-component/manage-product/manage-product.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ProductService } from 'src/app/services/product.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constants';
+import { ProductComponent } from '../dialog/product/product.component';
+
+@Component({
+  selector: 'app-manage-product',
+  templateUrl: './manage-product.component.html',
+  styleUrls: ['./manage-product.component.scss']
+})
+export class ManageProductComponent implements OnInit {
+  displayedColumns:string[]=['name','categoryName','description','price','edit'];
+  dataSource:any;
+  responseMessage:any;
+  constructor(private productService:ProductService,
+    private ngxService:NgxUiLoaderService,
+    private dialog:MatDialog,
+    private snackBar:SnackbarService,
+    private router:Router ) { }
+    
+  ngOnInit(): void {
+    this.ngxService.start();
+    this.tableData();
+  }
+
+  tableData(){
+    this.productService.getProduct().subscribe((response:any)=>{
+      this.ngxService.stop();
+      this.dataSource = new MatTableDataSource(response);
+      },(error)=>{
+        this.ngxService.stop();
+        console.log(error);
+        if(error.error?.message){
+          this.responseMessage = error.error?.message;
+        }else{
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackBar.openSnackBar(this.responseMessage,GlobalConstants.error);
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
+  handleAddAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      action: 'Add'
+    }
+    dialogConfig.width = "850px";
+    const dialogRef = this.dialog.open(ProductComponent,dialogConfig);
+    this.router.events.subscribe(() => {
+      dialogRef.close();
+    });
+    const sub = dialogRef.componentInstance.onAddProduct.subscribe((response:any)=>{
+        this.tableData();
+    })
+  }
+
+  handleEditAction(values:any){ 
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      action: 'Edit',
+      data: values
+    }
+    dialogConfig.width = "850px";
+    const dialogRef = this.dialog.open(ProductComponent,dialogConfig);
+    this.router.events.subscribe(() => {
+      dialogRef.close();
+    });
+    const sub = dialogRef.componentInstance.onEditProduct.subscribe((response:any)=>{
+        this.tableData();
+    })
+  }
+
+  handleDeleteAction(data:any){
+    
+  }
+
+  onChange(status:any,id:any){
+    
+  }
+}
