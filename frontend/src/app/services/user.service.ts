ec08ce74cd7d383f27cd3fea455d3e8c896aff83
Bhaveshgupta00 <bhaveshguptanit@gmail.com>
2025-04-16 16:37:48 +0530

    User View is also Done project is completed till now

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -40,7 +40,16 @@ export class UserService {
     return this.httpClient.post(this.url +
       "/user/changePassword",data,{
         headers: new HttpHeaders().set('Content-Type',"application/json")
-      }
-    );
+      })
+  }
+
+  getUsers(){
+    return this.httpClient.get(this.url+"/user/get/");
+  }
+
+  update(data:any){
+    return this.httpClient.patch(this.url+"/user/update/",data,{
+      headers: new HttpHeaders().set('Content-Type',"application/json")
+    });
   }
 }
